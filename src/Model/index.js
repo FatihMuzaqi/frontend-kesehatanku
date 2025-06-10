3c1d051a5000175a53ac5ad86c0f00bead3f015f
FatihMuzaqi <fatih.muzaqi123@gmail.com>
2025-06-11 01:15:22 +0700

    push

--- src/Model/index.js
+++ src/Model/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL = import.meta.env.VITE_API_URL;
 
 export const instance = axios.create({
-    baseURL: baseURL,
+    baseURL: 'https://backend-kesehatanku-production.up.railway.app',
     timeout: 60000,
     headers: {
         "Content-Type": "application/json"
